db2c7e56e3251247d163bdb3cc8583de75459b48
freijomil <mfreijomil@udesa.edu.ar>
2024-06-19 16:18:22 -0300

    login y data storage terminados, queda hacerlo mas lindo, saludo en index terminado

--- js/login.js
+++ js/login.js
@@ -1,37 +1,44 @@
 let login = document.querySelector(".login")
 
-let password = document.querySelector("#password")
 
-let mail = document.querySelector("#email")
 
 mensajeContra = "Por favor complete el campo contraseña"
 mensajeMail = "Por favor complete el campo mail"
 mensajeLong ="La contraseña debe tener al menos 6 caracteres"
+let todoOk = true
 login.addEventListener("submit", function(e){
     e.preventDefault()
+    let password = document.querySelector("#password").value
+    let mail = document.querySelector("#email").value
     
-    let todoOk = true
+    
+    if (email === '' || password === '') {
+        todoOk = false
+        alert('El correo electrónico y la contraseña son obligatorios.');
+        return;
+    }
 
-    if (password.value = "" ){
+    if (password.length < 6) {
         todoOk = false
-        alert(mensajeContra)
-    }  else if(password.length< 6){
-       alert(mensajeLong)
+        alert('La contraseña debe tener al menos 6 caracteres.');
+        return;
     }
-  
-    
-    if (mail.value = "" ){
-        todoOk = false
-        alert(mensajeMail)
-    }
-    
-        
-    
-    if (todoOK){
+       
+    if (todoOk){
         this.submit()
     }
+    let dataLogin = {
+        mail: mail,
+        contra: password
+    }
+    localStorage.setItem('dataLogin', JSON.stringify(dataLogin));
+
+    window.location.href = "index.html"
 })
 
-localStorage.setItem("mail", mail)
-
-localStorage.setItem("password", password)
+let dataGuardada= localStorage.getItem('dataLogin');
+if (dataGuardada) {
+    let savedData = JSON.parse(dataGuardada);
+    document.querySelector('#email').value = savedData.email;
+    document.querySelector('#password').value = savedData.password;
+}
